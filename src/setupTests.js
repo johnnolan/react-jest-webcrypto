2695b6122c9350a2f36764237bcd2ec3a31dfd4b
John Nolan <john.nolan@surevine.com>
2019-06-15 16:20:28 +0100

    (refactor) Removed unneeded atob library

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,8 @@
+// WebCrypto functionality requires the ability to encode and decode text and access it from the window.
 import encoding from 'text-encoding'
 window.TextEncoder = encoding.TextEncoder
 window.TextDecoder = encoding.TextDecoder
-window.atob = require('atob')
+
+// Gives us access to a implementation of WebCrypto in Node which is available in the broswer but missing on the command line.
 var WebCrypto = require('node-webcrypto-ossl')
 window.crypto = new WebCrypto()
