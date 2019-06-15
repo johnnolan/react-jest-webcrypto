f34a2c322c67b6c145a1c1d9589365f07fcd1c5e
John Nolan <john.nolan@surevine.com>
2019-06-15 17:12:14 +0100

    Updates

---
+++ precache-manifest.29e5d129b8fc2a655481d6194d9c9c54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a15ae1a2a36565051088d9d52fb98858",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5e230e785409ed599025",
+    "url": "/static/css/main.f62e0562.chunk.css"
+  },
+  {
+    "revision": "aac6ec2be8770a0d8523",
+    "url": "/static/js/2.2c8e99cf.chunk.js"
+  },
+  {
+    "revision": "5e230e785409ed599025",
+    "url": "/static/js/main.8b9c1d03.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-jest-webcrypto/precache-manifest.430ffd2330a3199f1eea7325e767d8e9.js"
+  "/precache-manifest.29e5d129b8fc2a655481d6194d9c9c54.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-jest-webcrypto/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
